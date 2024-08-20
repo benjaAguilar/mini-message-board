86bbc27a913ba2f02f62aff53928008cfafbc11a
Benja Aguilar <benjaaguilar02@gmail.com>
2024-08-20 19:16:00 -0300

    chore: add dotenv config

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('node:path');
 const app = express();
+require('dotenv').config({
+    path: process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev'
+});
 
 const indexRoute = require('./routes/indexRoute');
 
@@ -10,8 +13,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRoute);
 
