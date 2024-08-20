1df35ef6449bd12c41a1b4d88a47057c0f45755d
Benja Aguilar <benjaaguilar02@gmail.com>
2024-08-20 19:18:51 -0300

    feat: db files and configs

---
+++ db/pool.js
@@ -0,0 +1,9 @@
+const { Pool } = require("pg");
+
+module.exports = new Pool({
+  host: process.env.HOST,
+  user: process.env.USER_NAME,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT 
+});
---
+++ db/populatedb.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+const { Client } = require("pg");
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev'
+});
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  username VARCHAR(255),
+  message VARCHAR(255),
+  date DATE DEFAULT CURRENT_DATE,
+  likes INT DEFAULT 0
+);
+
+INSERT INTO messages (username, message) 
+VALUES
+  ('ColorCode', 'Now we have DB integration! hope you like <3'),
+  ('Benja12', 'Kiteboarding is awesome!'),
+  ('Rick Sanchez', 'Im pickle rick!');
+
+`;
+
+async function main() {
+  console.log("seeding...");
+  console.log(process.env.POPULATE_CONNECTION)
+  const client = new Client({
+    connectionString: process.env.POPULATE_CONNECTION,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
