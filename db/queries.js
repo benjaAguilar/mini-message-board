9c95d4a70f6e87d68c92fbf027efb178eff069b0
Benja Aguilar <benjaaguilar02@gmail.com>
2024-08-21 11:11:51 -0300

    feat: query to create and update likes

--- db/queries.js
+++ db/queries.js
@@ -5,6 +5,19 @@ async function getAllMessages(){
     return rows;
 }
 
+async function createNewMessage(message, user){
+    await pool.query("INSERT INTO messages (message, username) VALUES($1, $2)", [message, user]);
+}
+
+async function increaseLikes(id){
+    await pool.query(
+        `UPDATE messages SET likes = likes + 1                
+        WHERE id = $1
+        `, [id]);
+}
+
 module.exports = {
-    getAllMessages
+    getAllMessages,
+    createNewMessage,
+    increaseLikes
 }
