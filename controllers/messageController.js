32f349f0d4809fdfdc00a2e5b5363bfd5a5a98d3
Benja Aguilar <benjaaguilar02@gmail.com>
2024-08-22 08:32:01 -0300

    feat: orderBy controller
    this function gets the order by chossen by the
    user

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -18,8 +18,22 @@ async function postUpdateLikes(req, res){
     res.json({success: true});
 }
 
+async function sortBy(req, res){
+    const { sorted } = req.query;
+    let filter;
+    
+    if(sorted === 'newest') filter = "ORDER BY date DESC";
+    if(sorted === 'oldest') filter = "ORDER BY date";
+    if(sorted === 'name') filter = "ORDER BY username";
+
+    const messages = await db.getAllMessages(filter);
+
+    res.render('index', {msg: messages});
+}
+
 module.exports = {
     getIndex,
     postMessage, 
-    postUpdateLikes
+    postUpdateLikes,
+    sortBy
 }
